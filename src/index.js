ab1a52b40d99924fcf81f82a6dac670cb82f0843
alizahir23 <alizahiruddin@gmail.com>
2020-10-18 18:28:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import Header from './Components/Header'
 import App from './App';
 
+// THE FOLLOWING CHANGES ARE BEING MADE IN ORDER TO PARTICIPATE IN HACKTOBERFEST
 
 ReactDOM.render(
   <React.StrictMode>
-
     <Header />
     <App />
   </React.StrictMode>,
